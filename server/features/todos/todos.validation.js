79ebf2b98b405147c93d637db021e863b4a2242c
Akhtar, Murtaza <ma2106@hw.ac.uk>
2022-06-02 09:32:40 +0100

    minor changes

--- server/features/todos/todos.validation.js
+++ server/features/todos/todos.validation.js
@@ -4,15 +4,12 @@ export const setIncludeTodoIdValidationRules = () => [
   param('id', 'Invalid ID').not().isEmpty()
 ];
 
-// TODO new Date().getTimezoneOffset() = -60
-// eslint-disable-next-line max-len
-// TODO https://stackoverflow.com/questions/1091372/getting-the-clients-time-zone-and-offset-in-javascript
 export const setAddTodoValidationRules = () => [
   body('title', 'Please provide a title').not().isEmpty(),
   body('description', 'Please provide a valid description')
     .optional({ nullable: true }).not().isEmpty(),
   body('deadlineDate', 'Please provide a valid date')
-    .optional({ nullable: true }).toDate(),
+    .optional({ nullable: true }).isNumeric().toDate(),
   body('tags', 'Please provide an array of tags which are to be in String format')
     .optional({ nullable: true }).isArray(),
   body('important', '"Important" may only be true or false')
