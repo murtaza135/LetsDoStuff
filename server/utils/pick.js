ac8884159512cdf2f6554434979e0fee136381d7
Akhtar, Murtaza <ma2106@hw.ac.uk>
2022-06-03 01:24:37 +0100

    minor cleanup

--- server/utils/pick.js
+++ server/utils/pick.js
@@ -1,10 +1,10 @@
 const pick = (obj, keys) => (
   keys
     .filter((key) => key in obj)
-    .reduce((newObj, key) => {
+    .reduce((result, key) => {
       // eslint-disable-next-line no-param-reassign
-      newObj[key] = obj[key];
-      return newObj;
+      result[key] = obj[key];
+      return result;
     }, {})
 );
 
