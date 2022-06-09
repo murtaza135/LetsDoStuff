d198f25eebbc4989cf5944efc35524d1aaff7c08
Akhtar, Murtaza <ma2106@hw.ac.uk>
2022-06-09 23:43:02 +0100

    minor changes

--- client/src/global-components/form/form-field/FormInput.styles.js
+++ client/src/global-components/form/form-field/FormInput.styles.js
@@ -4,7 +4,7 @@ import { breakpoints, primaryTheme as theme } from 'constants';
 
 const FormInput = styled(Field)`
   color: ${theme.secondary};
-  background: ${theme.primaryShaded};
+  background-color: ${theme.primaryShaded};
   border: ${({ error }) => (error ? `2px solid ${theme.danger}` : `1px solid ${theme.secondary}`)};
   border-radius: 5px;
   outline: none !important;
@@ -14,10 +14,10 @@ const FormInput = styled(Field)`
   &:-webkit-autofill,
   &:-webkit-autofill:hover, 
   &:-webkit-autofill:focus, 
-  &:-webkit-autofill:active{
+  &:-webkit-autofill:active {
     -webkit-box-shadow: 0 0 0 100px ${theme.primaryShaded} inset !important;
     -webkit-text-fill-color: ${theme.secondary} !important;
-}
+  }
 
   @media screen and (max-width: ${breakpoints.sm}) {
     padding: 0.6rem;
