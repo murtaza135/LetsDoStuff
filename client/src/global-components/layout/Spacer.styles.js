b755aa44a14cc7363c71e4e4124129d19fa9786e
Akhtar, Murtaza <ma2106@hw.ac.uk>
2022-06-19 01:43:32 +0100

    created spacer mixin

--- client/src/global-components/layout/Spacer.styles.js
+++ client/src/global-components/layout/Spacer.styles.js
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 
-const marginMixin = css`
+const marginCSS = css`
   margin: ${({ $m }) => $m};
   margin-top: ${({ $mt, $my, $m }) => $mt || $my || $m};
   margin-right: ${({ $mr, $mx, $m }) => $mr || $mx || $m};
@@ -8,7 +8,7 @@ const marginMixin = css`
   margin-left: ${({ $ml, $mx, $m }) => $ml || $mx || $m};
 `;
 
-const paddingMixin = css`
+const paddingCSS = css`
   padding: ${({ $p }) => $p};
   padding-top: ${({ $pt, $py, $p }) => $pt || $py || $p};
   padding-right: ${({ $pr, $px, $p }) => $pr || $px || $p};
@@ -18,9 +18,14 @@ const paddingMixin = css`
 
 // @props $m | $mx | $my | $mt | $mr | $mb | $ml
 // @props $p | $px | $py | $pt | $pr | $pb | $pl
-const Spacer = styled.span`
-  ${marginMixin};
-  ${paddingMixin};
+export const spacerCSS = css`
+  ${marginCSS};
+  ${paddingCSS};
 `;
 
-export default Spacer;
+// @props $m | $mx | $my | $mt | $mr | $mb | $ml
+// @props $p | $px | $py | $pt | $pr | $pb | $pl
+export const Spacer = styled.span`
+  ${marginCSS};
+  ${paddingCSS};
+`;
