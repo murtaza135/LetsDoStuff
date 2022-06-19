c9f9e63b32d5417130a54d691b0029b755d624c9
Akhtar, Murtaza <ma2106@hw.ac.uk>
2022-06-19 23:58:36 +0100

    created NotFound page

--- client/src/features/not-found/NotFound.js
+++ client/src/features/not-found/NotFound.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
 import { Button } from 'global-components/ui';
 import notFoundImg from './not_found.png';
+import * as S from './NotFound.styles';
 
 const NotFoundError = () => (
   <Fragment>
-    <img src={notFoundImg} alt="Not Found" />
+    <S.NotFoundImage src={notFoundImg} alt="Not Found" />
     <Button to="/" $color="secondary" $size="s" $bold $mt="2rem">Go to Dashboard</Button>
   </Fragment>
 );
