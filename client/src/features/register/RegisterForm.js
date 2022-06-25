b84b68bef0a5cc666f0e43731b71394359f67f63
Akhtar, Murtaza <ma2106@hw.ac.uk>
2022-06-25 01:39:58 +0100

    added register api call in RegisterForm component

--- client/src/features/register/RegisterForm.js
+++ client/src/features/register/RegisterForm.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Formik, Form, FormTitle, FormInputGroup, FormButton } from 'global-components/form';
+import { useRegisterMutation } from 'features/auth/auth.apislice';
+import { useNavigate } from 'react-router-dom';
 import validator from './RegisterForm.validator';
 
 const initialValues = {
@@ -10,46 +12,62 @@ const initialValues = {
   confirmPassword: ''
 };
 
-const RegisterForm = () => (
-  <Formik
-    initialValues={initialValues}
-    validationSchema={validator}
-  >
-    <Form>
-      <FormTitle>Register</FormTitle>
-      <FormInputGroup
-        name="name"
-        type="text"
-        placeholder="Enter Name"
-        label="Name"
-      />
-      <FormInputGroup
-        name="email"
-        type="email"
-        placeholder="Enter Email"
-        label="Email"
-      />
-      <FormInputGroup
-        name="username"
-        type="text"
-        placeholder="Enter Username"
-        label="Username"
-      />
-      <FormInputGroup
-        name="password"
-        type="password"
-        placeholder="Enter Password"
-        label="Password"
-      />
-      <FormInputGroup
-        name="confirmPassword"
-        type="password"
-        placeholder="Confirm Password"
-        label="Confirm Password"
-      />
-      <FormButton type="submit">Register</FormButton>
-    </Form>
-  </Formik>
-);
+const RegisterForm = () => {
+  const navigate = useNavigate();
+  const [register] = useRegisterMutation();
+
+  const handleRegister = async (values) => {
+    try {
+      await register(values).unwrap();
+      navigate('/');
+    } catch (error) {
+      // TODO add error component
+      console.log(error);
+    }
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validator}
+      onSubmit={handleRegister}
+    >
+      <Form>
+        <FormTitle>Register</FormTitle>
+        <FormInputGroup
+          name="name"
+          type="text"
+          placeholder="Enter Name"
+          label="Name"
+        />
+        <FormInputGroup
+          name="email"
+          type="email"
+          placeholder="Enter Email"
+          label="Email"
+        />
+        <FormInputGroup
+          name="username"
+          type="text"
+          placeholder="Enter Username"
+          label="Username"
+        />
+        <FormInputGroup
+          name="password"
+          type="password"
+          placeholder="Enter Password"
+          label="Password"
+        />
+        <FormInputGroup
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm Password"
+          label="Confirm Password"
+        />
+        <FormButton type="submit">Register</FormButton>
+      </Form>
+    </Formik>
+  );
+};
 
 export default RegisterForm;
