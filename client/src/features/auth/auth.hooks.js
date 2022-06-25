286ca34af9cea49c25dd4efbb344f05c2e40a60b
Akhtar, Murtaza <ma2106@hw.ac.uk>
2022-06-25 02:31:13 +0100

    added useCallback to useLogout hook

--- client/src/features/auth/auth.hooks.js
+++ client/src/features/auth/auth.hooks.js
@@ -1,7 +1,8 @@
+import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from './auth.slice';
 
 export const useLogout = () => {
   const dispatch = useDispatch();
-  return () => dispatch(logout());
+  return useCallback(() => dispatch(logout()), [dispatch]);
 };
