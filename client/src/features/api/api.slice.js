8cab459e1c2fe6d496796734bc89ded27ad7b401
Akhtar, Murtaza <ma2106@hw.ac.uk>
2022-07-02 03:54:28 +0100

    prepared for deployment

--- client/src/features/api/api.slice.js
+++ client/src/features/api/api.slice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://mern-lets-do-stuff.herokuapp.com:3000/api',
+    baseUrl: 'https://mern-lets-do-stuff.herokuapp.com:5000/api',
     prepareHeaders: (headers, { getState }) => {
       const { token } = getState().auth;
       if (token) {
