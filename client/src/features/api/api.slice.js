84809e5102b41841e62fb41edf62ae60f65fdb0d
Akhtar, Murtaza <ma2106@hw.ac.uk>
2022-07-02 14:21:08 +0100

    prepared for deployment

--- client/src/features/api/api.slice.js
+++ client/src/features/api/api.slice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://lets-do-stuff.vercel.app/api',
+    baseUrl: 'http://localhost:5000/api',
     prepareHeaders: (headers, { getState }) => {
       const { token } = getState().auth;
       if (token) {
