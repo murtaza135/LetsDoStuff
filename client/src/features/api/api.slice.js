dd72b582b7ddb53d9a489bfae9bf2838ccd2111d
Akhtar, Murtaza <ma2106@hw.ac.uk>
2022-07-02 04:12:21 +0100

    prepared for deployment

--- client/src/features/api/api.slice.js
+++ client/src/features/api/api.slice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: '/api',
+    baseUrl: ':5000/api',
     prepareHeaders: (headers, { getState }) => {
       const { token } = getState().auth;
       if (token) {
